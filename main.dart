503c7f20f6124b16c08541fb9f43356114b10bda
Zohaib11117 <105278831+Zohaib11117@users.noreply.github.com>
2023-09-16 13:26:30 +0500

    my first commit

---
+++ main.dart
@@ -0,0 +1,155 @@
+void main() {
+  // Q: 1
+  // int len = 25;
+  // int bre = 25;
+  // if(len == bre) {
+  //   print('Square');
+  // }else{
+  //   print('Rectangle');
+  // }
+
+// Q: 2
+  // var a = 15;
+  // var b = 25;
+  // if (a <= b) {
+  //   print('Youngest');
+  // } else if (a >= b) {
+  //   print('oldest');
+  // }
+
+// Q: 3
+  // var held = 16;
+  // var attened = 12;
+  // var per = (attened / held) * 100;
+  // if (per >= 75) {
+  //   print('Yes Is Allowed sit in Exam');
+  // } else {
+  //   print('Is not allowed to sit in Exam');
+  // }
+
+  // Q: 4
+  // var year = 1900;
+  // if (year % 4 == 0) {
+  //   print('$year is a Leap year');
+  // } else {
+  //   print('$year is not a Leap year');
+  // }
+
+// Q: 5
+  // var tem = 45;
+  // if (tem <= 0) {
+  //   print('Freezing weather');
+  // } else if (tem <= 10) {
+  //   print('Very Cold weather');
+  // } else if (tem <= 20) {
+  //   print('Cold Weather');
+  // } else if (tem <= 30) {
+  //   print('Normal in Temperature');
+  // } else if (tem <= 40) {
+  //   print('Its Hot');
+  // } else if (tem >= 40) {
+  //   print('Very Hot');
+  // }
+
+  // Q: 6
+  // var cons = "E";
+  // if (cons == 'A' || cons == "E" || cons == "I" || cons == "O" || cons == "U") {
+  //   print("Vowel");
+  // } else {
+  //   print("Consonent");
+  // }
+
+// Q: 7
+  // var name = "james";
+  // var id = 1001;
+  // double num = unit * 2.00;
+  // var unit = 800;
+  // if (unit == 200) {
+  //   print('upto 199 Per Unit 1.20PKR');
+  // } else if (unit <= 200) {
+  //   print('200 and Above but Less Then 400 Per Unit 1.50PKR');
+  // } else if (unit <= 400) {
+  //   print('400 and Above but Less Then 600 Per unit 1.80PKR');
+  // } else if (unit >= 600) {
+  //   print('600 and Above Per Unit 2.00PKR');
+  // }
+
+  // print('ID: $id');
+  // print('Name: $name');
+  // print('Unit: $unit');
+  // print('Expected Output');
+  // print('Customer ID No: $id');
+  // print('Customer Name: $name');
+  // print('Unit Consumed: $unit');
+  // print('Amount Cherges: 2.00PKR Per Unit $num');
+  // print('Net bill Amount: $num');
+
+//   int num = 10;
+//   if (num < 10) {
+//     print('Number is Less than 10');
+//     if (num % 2 == 0) {
+//       print('Number is Even');
+//     }
+//   } else if (num > 10) {
+//     print('Number is Greater than 10');
+//   }
+
+  // var name = "Yousha";
+  // var Class = "X";
+  // var rollno = 779;
+  // var ob = 350;
+  // var total = 400;
+  // var per = (ob / total) * 100;
+  // print('Name: $name');
+  // print('Class: $Class');
+  // print('Roll Number: $rollno');
+  // print('Percentage: $per');
+  // if(per <= 60){
+  //   print('Grade: C');
+  // }else if(per <=70){
+  //   print('Garde: B');
+  // }else if(per <= 80){
+  //   print('Grade: A');
+  // }else if(per >= 80){
+  //   print('Grade: Aone');
+  // }
+
+  var names = ["Zohaib", "Jawad", "Yousha", "Summamah"];
+  var fruits = ["orange", "mango"];
+  // print(names[0]);
+  // print(names.length);
+  // print(names.reversed);
+  // print(names.first);
+  // print(names.last);
+  // print(names.isEmpty);
+  // print(names.isNotEmpty);
+  // print(names.elementAt(3));
+  // names.replaceRange(0, 3, ["Umer", "Ibrahim"]);
+  // print(names);
+  // names.replaceRange(0, names.length, ["Yousha"]);
+  // print(names);
+  // names.replaceRange(names.length - 1, names.length, []);
+  // print(names);
+  // names.sort();
+  // print(names);
+  // names.addAll(fruits);
+  // print(names);
+  // names.insert(1, "abc");
+  // print(names);
+  // names.insertAll(3, fruits);
+  // print(names);
+  // names.remove("Yousha");
+  // print(names);
+  // names.removeAt(names.length - 1);
+  // print(names);
+  // names.removeLast();
+  // print(names);
+  // names.clear();
+  // print(names);
+  // names.removeWhere((e) => e == "Yousha");
+  // print(names);
+  // names.removeRange(1, 3);
+  // print(names);
+  // names.retainWhere((e) => e == "Yousha");
+  // print(names);
+}
